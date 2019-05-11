01430a4070592aea05f212659f3c67fcdb109c79
Marc Poppy <poppy.marc@gmail.com>
2019-05-11 03:51:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,10 +4,10 @@ def badge_maker(name)
    "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(name_array)
+def batch_badge_creator(speakers)
   #returns an array of badge names
   badge = []
-  name_array.each { |name| badge << badge_maker(name)}
+  speakers.each { |name| badge << badge_maker(name)}
   badge
 end
 
